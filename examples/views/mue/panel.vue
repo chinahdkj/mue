dc7918624784f9fba9ac7b3b8a806af3e8f0931f
zhukaidi <zhukaidi@chinahddz.com>
2018-11-15 13:08:32 +0800

    mue-panel 修改

--- examples/views/mue/panel.vue
+++ examples/views/mue/panel.vue
@@ -1,16 +1,31 @@
 <template>
     <div>
-        <mue-panel title="自动撑开">
+        <mue-panel hairline="wide">
+            <div>
+                props:<br/>
+                title String 标题
+                <br/>
+                hairline String 顶部边框 可选值none,thin,normal,wide, 默认none<br/>
+                none=0px thin=1px normal=4px wide=6px
+                <br/>
+                slot:<br/>
+                tools 头部右侧插槽<br/>
+
+                不设置 title 和 tools 不显示头部
+            </div>
+        </mue-panel>
+
+        <mue-panel title="自动撑开" hairline="normal">
             <div v-for="i in 10" :key="i"
                  :style="{height: '20px', background: i % 2 == 0 ? 'blue': 'red'}"></div>
         </mue-panel>
 
-        <mue-panel style="height:150px;" title="设置高度">
+        <mue-panel style="height:150px;" title="设置高度" hairline="normal">
             <div v-for="i in 10" :key="i"
                  :style="{height: '20px', background: i % 2 == 0 ? 'blue': 'red'}"></div>
         </mue-panel>
 
-        <mue-panel style="height:150px;" title="带按钮">
+        <mue-panel style="height:150px;" title="带按钮" hairline="normal">
             <van-checkbox v-model="ischart" slot="tools">
                 <i slot="icon" slot-scope="props" class="iconfont color-primary"
                    style="font-size: 16px;"
--- packages/Panel/src/index.vue
+++ packages/Panel/src/index.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="mue-panel">
-        <div class="mue-panel-header">
+    <div class="mue-panel"
+         :class="[hairline == 'none' ? '' : ('mue-panel--hairline-' + hairline),
+         $slots.tools || title ? '' : 'mue-panel--no-header']">
+        <div v-if="hairline != 'none'" class="mue-hairline"
+             :class="'mue-hairline--' + hairline"></div>
+        <div class="mue-panel-header" v-if="$slots.tools || title ">
             <panel-title :title="title"/>
             <div class="mue-panel-tools">
                 <slot name="tools"></slot>
@@ -18,6 +22,13 @@
     export default {
         name: "MuePanel",
         props: {
+            hairline: {
+                type: String,
+                default: "none",
+                validator: function(v){
+                    return ["none", "thin", "normal", "wide"].findIndex(p => p === v) > -1;
+                }
+            },
             title: String
         },
         data(){
