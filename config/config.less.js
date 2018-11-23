aaa26d44fb98ec57ec9e872336beab918f1de2a6
zhukaidi <zhukaidi@chinahddz.com>
2018-11-23 14:45:51 +0800

    css-loader 支持less变量

---
+++ config/config.less.js
@@ -0,0 +1,49 @@
+module.exports = {
+    modifyVars: {
+        "font-size": "14px",
+        "font-size-large": "16px",
+
+        "light-white": "#fff",
+        "light-black": "#000",
+        "light-gray": "#757575",
+        "light-gray-light": "#999999",
+        "light-common": "#212121",
+        "light-primary": "#4796e3",
+
+        "light-background-common": "#fff",
+
+        // 边框颜色
+        "light-border-color-base": "#e0e0e0",
+        "light-border-color-light": "#f0f0f0",
+        "light-border-color-dark": "#cccccc",
+
+        "light-disable-color": "#bdbdbd",
+        "light-disable-background": "#f0f0f0",
+
+        // button
+        "button-font-size": "16px",
+        "button-height": "44px",
+        "button-mini-font-size": "14px",
+        "button-mini-padding": "6px 10px",
+
+        "light-button-background": "#f0f0f0",
+
+        "light-button-active-mask": "0.2",
+
+        // radio
+        "radio-size": "20px",
+        "radio-font-size": "14px",
+
+        // checkbox
+        "checkbox-size": "20px",
+        "checkbox-font-size": "14px",
+
+        //panel
+        "light-panel-background": "#fff",
+
+        // datatable
+        "table-th-font-size": "12px",
+        "light-table-th-background": "#ebebeb"
+
+    }
+};
