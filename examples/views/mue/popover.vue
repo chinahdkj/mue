60892ec1cc74fc8d8ba16e0255ffffe4c3e7ea78
zhukaidi <zhukaidi@chinahddz.com>
2019-02-25 16:40:23 +0800

    popover 指令模式重构

--- examples/views/mue/popover.vue
+++ examples/views/mue/popover.vue
@@ -10,15 +10,17 @@
         <br/>
 
         指令模式<br/>
-        指令名称为popover；arg为弹出popover的ref值；value为点击弹出按钮时触发的方法，可不绑定；示例 v-popover:pop="openPop"<br/>
+        指令名称为popover；arg为弹出popover的ref值 或者 获得弹出popover的方法，ref查找优先；value为点击弹出按钮时触发的方法，可不绑定；示例
+        v-popover:pop="openPop"<br/>
         指令模式的可以多个弹出按钮共用一个popover弹出，能有效降低页面的DOM数量，提高性能。
 
-        <mue-popover ref="pop" placement="bottom">
+        <mue-popover ref="pop" placement="bottom" append-to-body overlay>
             <div style="padding: 10px;">{{btn}}</div>
         </mue-popover>
 
         <van-button style="margin:20px;" type="primary" v-popover:pop="openPop">按钮1</van-button>
-        <van-button style="margin:20px;" type="primary" v-popover:pop="openPop">按钮2</van-button>
+        <van-button style="margin:20px;" type="primary" v-popover:getpop="openPop">按钮2
+        </van-button>
 
     </div>
 </template>
@@ -33,6 +35,9 @@
         methods: {
             openPop(e){
                 this.btn = e.innerText;
+            },
+            getpop(){
+                return this.$refs.pop;
             }
         }
     }
