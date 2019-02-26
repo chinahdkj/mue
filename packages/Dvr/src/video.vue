6c173848cd3500381bf7d0bf9043693a2d6aaa8a
zhukaidi <zhukaidi@chinahddz.com>
2019-02-26 16:09:02 +0800

    Dvr 组件修改

--- packages/Dvr/src/video.vue
+++ packages/Dvr/src/video.vue
@@ -2,19 +2,23 @@
     <div class="mue-dvr-video">
 
         <div class="mue-dvr-video__content" :style="{height: height + 'px', width: width + 'px'}"
-             v-loading="loading" @click="Stop">
+             v-loading="client && loading" @click="Stop" v-resize="resize">
 
-            <template v-if="!rtsp">
+            <template v-if="needUpdate">
+                <span>视频服务版本太低，请联系服务器管理员！</span>
+            </template>
+
+            <template v-else-if="!rtsp">
                 <van-icon v-if="$listeners.choose" name="add-o" @click="choose"/>
             </template>
 
             <template v-else>
                 <template v-if="!client">
                     <van-icon name="play-circle-o" @click.stop="Play"></van-icon>
+                    <img :src="getThumb()">
                 </template>
 
-                <img v-if="src || thumb" :src="src || thumb">
-
+                <canvas v-if="client" ref="canvas"></canvas>
             </template>
 
         </div>
@@ -33,60 +37,8 @@
 
 <script>
 
-    const Base64ArrayBuffer = (arrayBuffer) => {
-        let base64 = "";
-        let encodings = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
-        let bytes = new Uint8Array(arrayBuffer);
-        let byteLength = bytes.byteLength;
-        let byteRemainder = byteLength % 3;
-        let mainLength = byteLength - byteRemainder;
-
-        let a, b, c, d;
-        let chunk;
-
-        // Main loop deals with bytes in chunks of 3
-        for(let i = 0; i < mainLength; i = i + 3){
-            // Combine the three bytes into a single integer
-            chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
-
-            // Use bitmasks to extract 6-bit segments from the triplet
-            a = (chunk & 16515072) >> 18; // 16515072 = (2^6 - 1) << 18
-            b = (chunk & 258048) >> 12; // 258048   = (2^6 - 1) << 12
-            c = (chunk & 4032) >> 6; // 4032     = (2^6 - 1) << 6
-            d = chunk & 63;               // 63       = 2^6 - 1
-
-            // Convert the raw binary segments to the appropriate ASCII encoding
-            base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
-        }
-
-        // Deal with the remaining bytes and padding
-        if(byteRemainder === 1){
-            chunk = bytes[mainLength];
-
-            a = (chunk & 252) >> 2; // 252 = (2^6 - 1) << 2
-
-            // Set the 4 least significant bits to zero
-            b = (chunk & 3) << 4; // 3   = 2^2 - 1
-
-            base64 += encodings[a] + encodings[b] + '==';
-        }
-        else if(byteRemainder === 2){
-            chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
-
-            a = (chunk & 64512) >> 10; // 64512 = (2^6 - 1) << 10
-            b = (chunk & 1008) >> 4; // 1008  = (2^6 - 1) << 4
-
-            // Set the 2 least significant bits to zero
-            c = (chunk & 15) << 2; // 15    = 2^4 - 1
-
-            base64 += encodings[a] + encodings[b] + encodings[c] + '=';
-        }
-
-        return base64;
-    };
-
     import socketIo from "socket.io-client"
+    import uuid from "../../../src/utils/uuid";
 
     export default {
         name: "MueDvrVideo",
@@ -102,13 +54,9 @@
         data(){
             return {
                 client: null,
-                src: ""
+                needUpdate: false,
+                loading: false,
             };
-        },
-        computed: {
-            loading(){
-                return this.client && !this.src;
-            }
         },
         watch: {
             rtsp: {
@@ -123,25 +71,51 @@
             }
         },
         methods: {
+            getThumb(){
+                return `/socket.io.thumb?p=${this.rtsp}&t=${uuid(8)}`;
+            },
             choose(){
                 this.$emit("choose");
+            },
+            resize(){
+                this.client && this.Play();
             },
             Stop(){
                 if(this.client){
                     this.client.close();
                     this.client = null;
+                    this.loading = false;
                 }
             },
             Play(){
+                this.Stop();
+
                 if(!this.rtsp){
                     return;
                 }
 
-                this.client = socketIo("/ffmpeg", {query: {stream: this.rtsp}});
+                this.needUpdate = false;
+                this.client = socketIo("/ffmpeg", {
+                    query: {stream: this.rtsp, width: this.width, height: this.height}
+                });
+                this.loading = true;
 
                 this.client.on("DATA", (data) => {
-                    let bytes = new Uint8Array(data);
-                    this.src = `data:image/jpeg;base64,${Base64ArrayBuffer(bytes)}`;
+                    if(typeof data != "string"){
+                        this.needUpdate = true;
+                        this.Stop();
+                        return;
+                    }
+                    let img = new Image();
+                    img.onload = () => {
+                        this.loading = false;
+                        this.$refs.canvas.height = this.height;
+                        this.$refs.canvas.width = this.width;
+                        let ctx = this.$refs.canvas.getContext("2d");
+                        ctx.drawImage(img, 0, 0, this.width, this.height);
+                    };
+                    img.crossOrigin = "anonymous";
+                    img.src = data;
                 });
 
                 this.client.on("ERROR", (data) => {
