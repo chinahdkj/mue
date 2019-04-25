980e774e83de91b0548f54c15520ba3ca2f4c561
dafengshen <1548917336@qq.com>
2019-04-25 14:53:32 +0800

    追加原生方法

--- src/lib/native.js
+++ src/lib/native.js
@@ -9,7 +9,7 @@ const fns = [
     "hideHeader", "scanCode", "copy", "share", "userInfo", "userBehaviorRecord", "search",
     "collect", "nfcData", "goback", "refreshBadge", "signature","saveLocalData","queryLocalData","deleteLocalData",
     "bluetooth", "btDisConnected", "manualPost", "btGetParams", "manualPostRes","btSound","soundPlay","vibratorSound","dictList",
-    "btScan", "shareFile", "btUpdate", "spectrum"
+    "btScan", "shareFile", "btUpdate", "spectrum", "trace"
 ];
 
 const fns2 = ['search', 'collect', 'btDisConnected', 'btReceiver', 'btGetParams', 'manualPostRes','btSound','btScan', 'handCollectResp', 'bluetooth'] // 原生主动调用js的方法
