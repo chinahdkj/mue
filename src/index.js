92060a7a4f042b50e3624c800b359323d2338cae
zhukaidi <zhukaidi@chinahddz.com>
2019-07-03 11:59:44 +0800

    toast 位置修改

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
-import Chart from '../packages/Chart/index.js';
-import Panel from '../packages/Panel/index.js';
-import {RollData, RollDataGroup} from '../packages/RollData/index.js';
-import DatePicker from '../packages/DatePicker/index.js';
-import DateRangePicker from '../packages/DateRangePicker/index.js';
-import Search from '../packages/Search/index.js';
-import Popover from '../packages/Popover/index.js';
-import {Tab, Tabs} from '../packages/Tabs/index.js';
-import Loading from '../packages/Loading/index.js';
-import {Container, Footer, Header, Main} from '../packages/Container/index.js';
-import Page from '../packages/Page/index.js';
-import Datatable from '../packages/Datatable/index.js';
-import Form from '../packages/Form/index.js';
-import LoadMore from '../packages/LoadMore/index.js';
-import Dvr from '../packages/Dvr/index.js';
-import SortPicker from '../packages/SortPicker/index.js';
+import Chart from "../packages/Chart/index.js";
+import Panel from "../packages/Panel/index.js";
+import {RollData, RollDataGroup} from "../packages/RollData/index.js";
+import DatePicker from "../packages/DatePicker/index.js";
+import DateRangePicker from "../packages/DateRangePicker/index.js";
+import Search from "../packages/Search/index.js";
+import Popover from "../packages/Popover/index.js";
+import {Tab, Tabs} from "../packages/Tabs/index.js";
+import Loading from "../packages/Loading/index.js";
+import {Container, Footer, Header, Main} from "../packages/Container/index.js";
+import Page from "../packages/Page/index.js";
+import Datatable from "../packages/Datatable/index.js";
+import Form from "../packages/Form/index.js";
+import LoadMore from "../packages/LoadMore/index.js";
+import Dvr from "../packages/Dvr/index.js";
+import SortPicker from "../packages/SortPicker/index.js";
 import Tree from "../packages/Tree/index.js";
 import Empty from "../packages/Empty/index.js";
 //direcives
-import popover from '../packages/directives/popover.js';
-import resize from '../packages/directives/resize.js';
-import touch from '../packages/directives/touch.js';
+import popover from "../packages/directives/popover.js";
+import resize from "../packages/directives/resize.js";
+import touch from "../packages/directives/touch.js";
 
 
-import Vant, {Locale} from 'vant';
-import zhCN from 'vant/lib/locale/lang/zh-CN';
-import axios from 'axios'
-import http, {InitHttp} from './lib/http';
-import native from './lib/native';
-import comm from './lib/common';
+import Vant, {Locale} from "vant";
+import zhCN from "vant/lib/locale/lang/zh-CN";
+import axios from "axios";
+import http, {InitHttp} from "./lib/http";
+import native from "./lib/native";
+import comm from "./lib/common";
 
-import {objectGet} from './utils/object';
+import {objectGet} from "./utils/object";
 // svg 图标
 import "../themes/fonts/iconfont";
 
-Locale.use('zh-CN', zhCN);
+Locale.use("zh-CN", zhCN);
 
 window.requestAnimFrame = (() => {
     return window.requestAnimationFrame ||
@@ -61,6 +61,10 @@ const install = function(Vue){
     Vue.directive(resize.name, resize);
     Vue.directive(touch.name, touch);
 
+    if(Vue.prototype.$toast){
+        Vue.prototype.$toast.setDefaultOptions({position: "top"});
+    }
+
     Vue.prototype.$loading = Loading.service;
     Vue.prototype.$ajax = axios;
     InitHttp();
@@ -68,20 +72,21 @@ const install = function(Vue){
     Vue.prototype.$native = native;
     Vue.prototype.$comm = comm;
 
+
     // 过滤器
-    Vue.filter('OBJECT_GET', (object, expression) => {
-        return objectGet(object, expression)
+    Vue.filter("OBJECT_GET", (object, expression) => {
+        return objectGet(object, expression);
     });
 };
 
-if(typeof window !== 'undefined' && window.Vue){
+if(typeof window !== "undefined" && window.Vue){
     install(window.Vue);
 }
 
 window.MUE_VERSION = require("../package").version;
 
 export default {
-    version: '0.1.0',
+    version: "0.1.0",
     install,
     ...components
 };
