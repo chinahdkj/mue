e841ebe9e9ba0d67cb363aef2eab1d4a3e3fb97d
zhoubinchao <zhoubinchao@chinahdkj.com>
2019-08-09 17:33:46 +0800

    v88s新增方法

--- src/lib/native.js
+++ src/lib/native.js
@@ -11,13 +11,13 @@ const fns = [
     "bluetooth", "btDisConnected", "manualPost", "btGetParams", "manualPostRes","btSound","soundPlay","vibratorSound","dictList",
     "btScan", "shareFile", "btUpdate", "spectrum", "trace", "delFile", "interceptBack", "bgNavi",
     "video", "showVideo", "sound", "screenshot", "screenoff", "screenon", "bgNaviClose", "regeocode", "download", "watermarkCamera",
-    "download", "delFile", "sqlite_execsql", "sqlite_query", "multi_file"
+    "download", "delFile", "sqlite_execsql", "sqlite_query", "multi_file","v88s_zdsjcx","v88s_params","v88s_tc", "logger"
 ];
 
 const fns2 = [
     'search', 'collect', 'btDisConnected', 'btReceiver', 'btGetParams', 'manualPostRes', 'handPostResp', 
     'btSound','btScan', 'handCollectResp', 'bluetooth', 'interceptBack', "bgNavi", "getLocation", "btUpdate", 
-    "screenoff", "screenon", "bgNaviClose"
+    "screenoff", "screenon", "bgNaviClose", "btRawData"
 ] // 原生主动调用js的方法
 
 const postMessage = ({cb, method, params}) => {
