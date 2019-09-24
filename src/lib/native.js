3c9f94a27ec255a6a89ce6803dd6bb6b043fb6fd
zhukaidi <zhukaidi@chinahddz.com>
2019-09-24 17:05:36 +0800

    增加原生切换主题入口

--- src/lib/native.js
+++ src/lib/native.js
@@ -1,5 +1,5 @@
 // 原生调用方法
-import {isIos} from './common';
+import {isIos} from "./common";
 
 const _cache = {};
 const _cache2 = {};
@@ -7,28 +7,28 @@ const _cache2 = {};
 const fns = [
     "save", "delete", "query", "rotate", "startNavi", "getLocation", "isCollected", "sysInfo",
     "hideHeader", "scanCode", "copy", "share", "userInfo", "userBehaviorRecord", "search",
-    "collect", "nfcData", "goback", "refreshBadge", "signature","saveLocalData","queryLocalData","deleteLocalData",
-    "bluetooth", "btDisConnected",  "bluetoothState", "manualPost", "btGetParams", "manualPostRes","btSound","soundPlay","vibratorSound","dictList",
+    "collect", "nfcData", "goback", "refreshBadge", "signature", "saveLocalData", "queryLocalData", "deleteLocalData",
+    "bluetooth", "btDisConnected", "bluetoothState", "manualPost", "btGetParams", "manualPostRes", "btSound", "soundPlay", "vibratorSound", "dictList",
     "btScan", "shareFile", "btUpdate", "spectrum", "trace", "delFile", "interceptBack", "bgNavi",
     "video", "showVideo", "sound", "screenshot", "screenoff", "screenon", "bgNaviClose", "regeocode", "download", "watermarkCamera",
-    "download", "delFile", "sqlite_execsql", "sqlite_query", "multi_file","v88s_zdsjcx","v88s_params","v88s_tc", "logger", "singleDownload", "clearCache",
+    "download", "delFile", "sqlite_execsql", "sqlite_query", "multi_file", "v88s_zdsjcx", "v88s_params", "v88s_tc", "logger", "singleDownload", "clearCache",
     "resSave", "sqlite_close"
 ];
 
 const fns2 = [
-    'search', 'collect', 'btDisConnected', 'btReceiver', 'btGetParams', 'manualPostRes', 'handPostResp', 
-    'btSound','btScan', 'handCollectResp', 'bluetooth', "onBtState", 'interceptBack', "bgNavi", "getLocation", "btUpdate", 
+    "search", "collect", "btDisConnected", "btReceiver", "btGetParams", "manualPostRes", "handPostResp",
+    "btSound", "btScan", "handCollectResp", "bluetooth", "onBtState", "interceptBack", "bgNavi", "getLocation", "btUpdate",
     "screenoff", "screenon", "bgNaviClose", "btRawData", "onSingleDownload", "sqlite_close"
-] // 原生主动调用js的方法
+]; // 原生主动调用js的方法
 
 const postMessage = ({cb, method, params}) => {
     const msgid = parseInt(Math.random() * Math.pow(10, 17));
     _cache[msgid] = cb;
     try{
         isIos()
-        ? window.webkit.messageHandlers.postMessage.postMessage(
+            ? window.webkit.messageHandlers.postMessage.postMessage(
             JSON.stringify({msgid, method, params}))
-        : window.native.postMessage(
+            : window.native.postMessage(
             JSON.stringify({msgid, method, params}));
     } catch(e){
         console.log(e);
@@ -38,11 +38,11 @@ const postMessage = ({cb, method, params}) => {
 window.response = ({msgid, params, method}) => {
     // 原生回调传入一个json对象
     fns2.some(v => {
-        if (v === method) {
+        if(v === method){
             _cache2[method] = params;
-            return true
+            return true;
         }
-    })
+    });
     const cb = _cache[msgid];
     if(cb){
         let gon = cb(params);
@@ -75,7 +75,29 @@ let methods = {};
 fns.forEach((f) => {
     methods[f] = ({params, cb}) => {
         postMessage({method: f, params, cb});
-    }
+    };
 });
+
+// 原生传入改主题
+window.changeTheme = (t) => {
+    let hash = location.hash.split("?");
+    let query = hash[1] || "";
+    if(!query){
+        hash[1] = `theme=${t}`;
+        location.hash = hash.join("?");
+        return;
+    }
+    let params = {};
+    query = query.split("&");
+    for(let i = 0; i < query.length; i++){
+        let [k, v] = query[i].split("=");
+        params[k] = v;
+    }
+    params.theme = t;
+    hash[1] = Object.entries(params).map(([k, v]) => {
+        return `${k}=${v}`;
+    }).join("&");
+    location.hash = hash.join("?");
+};
 
 export default methods;
