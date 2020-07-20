d737194339031ebdb8b2f7d1cd4a4710f53a236f
shenbinbin <583381696@qq.com>
2020-07-20 10:25:07 +0800

    mue-input修复number模式下在ios系统无法输入小数点

--- packages/Form/blue-alarm/index.vue
+++ packages/Form/blue-alarm/index.vue
@@ -5,7 +5,7 @@
             <source src="../../../themes/audio/alarm.mp3" type="audio/mpeg">
         </audio>
 
-        <div class="play_audio" >
+        <div class="play_audio">
             <i v-if="!playing" class="play iconfont icon-xiangzuo"></i>
             <i v-else class="play fa fa-pause" @click="stopPlay"></i>
         </div>
@@ -65,7 +65,9 @@
             //检测数据，发起警报
             _checkAlarm() {
                 //ios下原生audio无法自动触发play方法
+                alert("进入报警");
                 if(this.$comm.isIos()) {
+                    alert("是ios")
                     console.log(window.location);
                     let idx = window.location.href.indexOf('/index.html#/');
                     if(idx > -1) {
@@ -77,6 +79,8 @@
                         this.$toast("报警音频资源未找到");
                     }
                     return
+                } else {
+                    alert("是安卓")
                 }
 
                 let $dom = this.$el.getElementsByClassName("audio")[0]
