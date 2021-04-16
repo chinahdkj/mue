ec49510f9788c56cb65ffaa48749fc0178446eba
shenbinbin <583381696@qq.com>
2021-04-16 09:27:19 +0800

    测试ios打包bug

--- src/lib/native.js
+++ src/lib/native.js
@@ -58,8 +58,8 @@ const postMessage = ({cb, method, params}) => {
     }
 };
 window.response = ({msgid, method, params}) => {
-    console.log("返回结果11111", params)
-    console.log("方法名", method)
+    // console.log("返回结果11111", params)
+    // console.log("方法名", method)
     // 原生回调传入一个json对象（id, 方法名，返回数据）
     fns2.some(v => {
         if(v === method){
