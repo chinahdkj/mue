a879a96078d0b14f43e8bb4ac5f26b421f2530a2
shenbinbin <shenbinbin@chinahddz.com>
2021-07-02 15:47:35 +0800

    ios输入框输入聚焦不灵敏问题

--- src/utils/fast-click.js
+++ src/utils/fast-click.js
@@ -1,6 +1,21 @@
 import FastClick from "fastclick";
+import {isIos} from "../lib/common";
 
 const instance = FastClick.attach(document.body);
+
+//处理ios输入框输入聚焦不灵敏问题
+FastClick.prototype.focus = function(targetElement) {
+    let length;
+    //兼容处理:在iOS7中，有一些元素（如date、datetime、month等）在setSelectionRange会出现TypeError
+    if (isIos() && targetElement.setSelectionRange && targetElement.type.indexOf('date') !== 0 && targetElement.type !== 'time' && targetElement.type !== 'month' && targetElement.type !== 'email') {
+        length = targetElement.value.length;
+        targetElement.setSelectionRange(length, length);
+        //修复bug ios 11.3不弹出键盘，这里加上聚焦代码，让其强制聚焦弹出键盘
+        targetElement.focus();
+    } else {
+        targetElement.focus();
+    }
+};
 
 export const ElementClick = (el) => {
     if(!el){
